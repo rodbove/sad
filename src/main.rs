00da42b8325699972117736cb59042269a6b0401
Rodrigo Mello <rodbove@gmail.com>
2021-07-12 20:52:03 -0300

    refactor: replace structopt for clap

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,25 @@
 use std::path::PathBuf;
-use structopt::StructOpt;
+use clap::Clap;
 use anyhow::{Context, Result};
 
-#[derive(StructOpt, Debug)]
-struct Opt {
-    #[structopt(short="e", long="exec")]
-    command: String,
-    stream: PathBuf
+#[derive(Clap, Debug)]
+struct Opts {
+    /// Text script to be executed on file
+    script: String,
+    /// File path to execute the script on
+    file: PathBuf,
+    /// Edit file in place or make a backup if a suffix is provided
+    #[clap(short, long)]
+    in_place: bool
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let opts = Opt::from_args();
-    let content = std::fs::read_to_string(&opts.stream)
-        .with_context(|| format!("Unable to read content from file {:?}", &opts.stream))?;
+    let opts = Opts::parse();
+    println!("{}", opts.in_place);
+    let content = std::fs::read_to_string(&opts.file)
+        .with_context(|| format!("Unable to read content from file {:?}", &opts.file))?;
 
-    let exec_args: Vec<&str> = opts.command.split("/").collect();
+    let exec_args: Vec<&str> = opts.script.split("/").collect();
 
     sad::handle_exec_command(&content, &exec_args);
 
